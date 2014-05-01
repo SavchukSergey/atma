f04960214907c017e8a65a9491469de282c42926
Sergey Savchuk <savchuk.sergey@gmail.com>
2014-05-01 19:20:46 +0300

    tests

--- Atmega.Asm.Tests/Tokens/TokenizerTests.cs
+++ Atmega.Asm.Tests/Tokens/TokenizerTests.cs
@@ -78,5 +78,32 @@ namespace Atmega.Asm.Tests.Tokens {
             Assert.AreEqual("dw", tokens.Read(TokenType.Literal).StringValue);
             Assert.AreEqual(0x123, tokens.Read(TokenType.Integer).IntegerValue);
         }
+
+        [Test]
+        public void MultilineTestTest() {
+            var tokens = Tokenize(@"
+db 2
+dw 123h, \
+    45h, \
+   'asd' 
+db 3
+");
+            Assert.AreEqual(14, tokens.Count);
+
+            tokens.Read(TokenType.NewLine);
+            Assert.AreEqual(2, tokens.Read(TokenType.Literal).Position.Line);
+            Assert.AreEqual(2, tokens.Read(TokenType.Integer).Position.Line);
+            tokens.Read(TokenType.NewLine);
+            Assert.AreEqual(3, tokens.Read(TokenType.Literal).Position.Line);
+            Assert.AreEqual(3, tokens.Read(TokenType.Integer).Position.Line);
+            Assert.AreEqual(3, tokens.Read(TokenType.Punctuation).Position.Line);
+            Assert.AreEqual(4, tokens.Read(TokenType.Integer).Position.Line);
+            Assert.AreEqual(4, tokens.Read(TokenType.Punctuation).Position.Line);
+            Assert.AreEqual(5, tokens.Read(TokenType.String).Position.Line);
+            tokens.Read(TokenType.NewLine);
+            Assert.AreEqual(6, tokens.Read(TokenType.Literal).Position.Line);
+            Assert.AreEqual(6, tokens.Read(TokenType.Integer).Position.Line);
+            tokens.Read(TokenType.NewLine);
+        }
     }
 }
