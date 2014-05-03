bf38cd6d1c48a5d32921ac7fbf0d0f270b5232cc
Sergey Savchuk <savchuk.sergey@gmail.com>
2014-05-03 23:31:56 +0300

    hex file

---
+++ Atmega.Asm.Tests/Hex/HexFileLineTest.cs
@@ -0,0 +1,22 @@
+﻿using Atmega.Asm.Hex;
+using NUnit.Framework;
+
+namespace Atmega.Asm.Tests.Hex {
+    [TestFixture]
+    public class HexFileLineTest {
+
+        [Test]
+        public void ParseTest() {
+            const string line = ":100B4C00E356EE0F94D210F4689440F81BD2E3569F";
+            var res = HexFileLine.Parse(line);
+
+            Assert.IsNotNull(res.Data);
+            Assert.AreEqual(16, res.Data.Length);
+            Assert.AreEqual(0xb4c, res.Address);
+            Assert.AreEqual(HexFileLineType.Data, res.Type);
+
+            Assert.AreEqual(0xe3, res.Data[0]);
+            Assert.AreEqual(0x56, res.Data[15]);
+        }
+    }
+}
---
+++ Atmega.Asm/Hex/HexFileLine.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atmega.Asm.Hex {
+    public class HexFileLine {
+
+        public ushort Address { get; set; }
+
+        public byte[] Data { get; set; }
+
+        public HexFileLineType Type { get; set; }
+
+        public static HexFileLine Parse(string line) {
+            if (!line.StartsWith(":")) throw new ArgumentException();
+            var pointer = 1;
+            var cnt = ReadHexByte(ref line, ref pointer);
+            var addr = ReadHexUShort(ref line, ref pointer);
+            var type = (HexFileLineType)ReadHexByte(ref line, ref pointer);
+            var array = new byte[cnt];
+            for (var i = 0; i < cnt; i++) {
+                array[i] = ReadHexByte(ref line, ref pointer);
+            }
+            var checksum = ReadHexByte(ref line, ref pointer);
+
+            return new HexFileLine {
+                Address = addr,
+                Data = array,
+                Type = type
+            };
+        }
+
+        private static ushort ReadHexUShort(ref string line, ref int start) {
+            var h = ReadHexByte(ref line, ref start);
+            var l = ReadHexByte(ref line, ref start);
+            return (ushort)((h << 8) + l);
+        }
+
+        private static byte ReadHexByte(ref string line, ref int start) {
+            var h = GetHexDigit(line[start++]);
+            var l = GetHexDigit(line[start++]);
+            return (byte)((h << 4) + l);
+        }
+
+        private static byte GetHexDigit(char ch) {
+            if (ch >= '0' && ch <= '9') return (byte)(ch - '0');
+            if (ch >= 'A' && ch <= 'Z') return (byte)(ch - 'A' + 10);
+            if (ch >= 'a' && ch <= 'z') return (byte)(ch - 'a' + 10);
+            throw new ArgumentException("Invalid character " + ch);
+        }
+    }
+}
