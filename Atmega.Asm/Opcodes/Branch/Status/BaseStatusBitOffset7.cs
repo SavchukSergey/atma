59ea63c3d4fcd90796957d7f0826a81c3f01c22f
Sergey Savchuk <savchuk.sergey@gmail.com>
2014-09-02 21:29:49 +0300

    branch opcodes

--- Atmega.Asm/Opcodes/Branch/Status/BaseStatusBitOffset7.cs
+++ Atmega.Asm/Opcodes/Branch/Status/BaseStatusBitOffset7.cs
@@ -1,13 +1,16 @@
 ﻿using Atmega.Asm.Tokens;
 
-namespace Atmega.Asm.Opcodes.Branch {
-    public abstract class BaseOffset7Opcode : BaseOpcode {
-
-        protected BaseOffset7Opcode(string opcodeTemplate)
+namespace Atmega.Asm.Opcodes.Branch.Status {
+    public abstract class BaseStatusBitOffset7 : BaseOpcode {
+        
+        protected BaseStatusBitOffset7(string opcodeTemplate)
             : base(opcodeTemplate) {
         }
 
         public override void Compile(AsmParser parser, AsmSection output) {
+            var bit = parser.ReadBit();
+            parser.ReadToken(TokenType.Comma);
+
             Token firstToken;
             var offset = parser.CalculateExpression(out firstToken);
             var zeroOffset = output.Offset + 2;
@@ -19,7 +22,8 @@ namespace Atmega.Asm.Opcodes.Branch {
             if (delta > 63 || delta < -64) {
                 throw new TokenException("relative jump out of range (-64; 63)", firstToken);
             }
-            var translation = new OpcodeTranslation { Opcode = _opcodeTemplate, Offset7 = (sbyte)delta };
+            
+            var translation = new OpcodeTranslation {Opcode = _opcodeTemplate, Offset7 = (sbyte) delta, BitNumber = bit};
             output.EmitCode(translation.Opcode);
         }
     }
