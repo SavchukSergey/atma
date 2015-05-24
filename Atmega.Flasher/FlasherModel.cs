86a5b4a3c566b4c26fa2fb5fc1cc126f800b17c7
Sergey Savchuk <savchuk.sergey@gmail.com>
2015-05-24 20:19:43 +0300

    flasher

---
+++ Atmega.Flasher/FlasherModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Text;
+using Atmega.Asm.Hex;
+
+namespace Atmega.Flasher {
+    public class FlasherModel : INotifyPropertyChanged {
+
+        private HexFile _hexFile = new HexFile();
+
+        public void OpenFile(string filePath) {
+            _hexFile = HexFile.Load(filePath);
+            OnPropertyChanged("HexFormatted");
+        }
+
+        public HexFile HexFile {
+            get {
+                return _hexFile;
+            }
+        }
+
+        public string HexFormatted {
+            get {
+                var sb = new StringBuilder();
+                foreach (var line in _hexFile.Lines) {
+                    sb.AppendFormat("{0:x4}:", line.Address);
+                    foreach (var bt in line.Data) {
+                        sb.AppendFormat("{0:x2} ", bt);
+                    }
+                    sb.AppendLine();
+                }
+                return sb.ToString();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName) {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Atmega.Flasher/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Win32;
+
+namespace Atmega.Flasher {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        private readonly FlasherModel _model = new FlasherModel();
+
+        public FlasherModel Model { get { return _model; } }
+
+        public MainWindow() {
+            InitializeComponent();
+            DataContext = _model;
+        }
+
+        private void OpenCommand(object sender, ExecutedRoutedEventArgs e) {
+            var dlg = new OpenFileDialog {
+                Filter = "Intel Hex File (.hex)|*.hex"
+            };
+
+            var result = dlg.ShowDialog();
+
+            if (result == true) {
+                _model.OpenFile(dlg.FileName);
+            }
+        }
+    }
+}
