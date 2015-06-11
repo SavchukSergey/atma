4b0512c9961528e1d927c9e19642152fdae63d7c
Sergey Savchuk <savchuk.sergey@gmail.com>
2015-06-11 22:23:15 +0300

    serial monitor

--- Atmega.Flasher.Wpf/Views/SerialMonitor/SerialMonitorWindow.xaml.cs
+++ Atmega.Flasher.Wpf/Views/SerialMonitor/SerialMonitorWindow.xaml.cs
@@ -106,69 +106,72 @@ namespace Atmega.Flasher.Views.SerialMonitor {
         }
 
 
-        private bool _altMode = false;
-        private int _altChar = 0;
+        private bool _altMode;
+        private int? _altChar;
+        
         private void MessageToSend_OnPreviewKeyDown(object sender, KeyEventArgs e) {
-            if (e.Key == Key.LeftCtrl) {
-                _altMode = true;
-                e.Handled = true;
-            } else if (_altMode) {
-                switch (e.Key) {
-                    case Key.D0:
-                    case Key.NumPad0:
-                        _altChar = _altChar * 10 + 0;
-                        break;
-                    case Key.D1:
-                    case Key.NumPad1:
-                        _altChar = _altChar * 10 + 1;
-                        break;
-                    case Key.D2:
-                    case Key.NumPad2:
-                        _altChar = _altChar * 10 + 2;
-                        break;
-                    case Key.D3:
-                    case Key.NumPad3:
-                        _altChar = _altChar * 10 + 3;
-                        break;
-                    case Key.D4:
-                    case Key.NumPad4:
-                        _altChar = _altChar * 10 + 4;
-                        break;
-                    case Key.D5:
-                    case Key.NumPad5:
-                        _altChar = _altChar * 10 + 5;
-                        break;
-                    case Key.D6:
-                    case Key.NumPad6:
-                        _altChar = _altChar * 10 + 6;
-                        break;
-                    case Key.D7:
-                    case Key.NumPad7:
-                        _altChar = _altChar * 10 + 7;
-                        break;
-                    case Key.D8:
-                    case Key.NumPad8:
-                        _altChar = _altChar * 10 + 8;
-                        break;
-                    case Key.D9:
-                    case Key.NumPad9:
-                        _altChar = _altChar * 10 + 9;
-                        break;
+            if (e.Key == Key.System) {
+                var digit = GetDigit(e.SystemKey);
+                if (digit.HasValue) {
+                    _altMode = true;
+                    e.Handled = true;
+                    _altChar = (_altChar ?? 0) * 10 + digit.Value;
                 }
-                e.Handled = true;
             }
         }
 
+
         private void MessageToSend_OnPreviewKeyUp(object sender, KeyEventArgs e) {
-            if (e.Key == Key.LeftCtrl) {
-                e.Handled = true;
-                int caretIndex = MessageToSend.CaretIndex;
-                MessageToSend.Text = MessageToSend.Text.Insert(caretIndex, ((char)_altChar).ToString());
-                Console.WriteLine("char: {0}", ((char)_altChar).ToString());
-                MessageToSend.CaretIndex = caretIndex + 1;
-                _altChar = 0;
-                _altMode = false;
+            if (e.Key == Key.LeftAlt || e.Key == Key.RightAlt) {
+                if (_altMode) {
+                    _altMode = false;
+                    if (_altChar.HasValue) {
+                        var caretIndex = MessageToSend.CaretIndex;
+                        MessageToSend.Text = MessageToSend.Text.Insert(caretIndex, ((char)_altChar.Value).ToString());
+                        MessageToSend.CaretIndex = caretIndex + 1;
+                        _altChar = null;
+                    }
+                }
             }
         }
+
+        private static int? GetDigit(Key key) {
+            switch (key) {
+                case Key.D0:
+                case Key.NumPad0:
+                    return 0;
+                case Key.D1:
+                case Key.NumPad1:
+                    return 1;
+                case Key.D2:
+                case Key.NumPad2:
+                    return 2;
+                case Key.D3:
+                case Key.NumPad3:
+                    return 3;
+                case Key.D4:
+                case Key.NumPad4:
+                    return 4;
+                case Key.D5:
+                case Key.NumPad5:
+                    return 5;
+                case Key.D6:
+                case Key.NumPad6:
+                    return 6;
+                case Key.D7:
+                case Key.NumPad7:
+                    return 7;
+                case Key.D8:
+                case Key.NumPad8:
+                    return 8;
+                case Key.D9:
+                case Key.NumPad9:
+                    return 9;
+                default:
+                    return null;
+            }
+        }
+
+
     }
 }
