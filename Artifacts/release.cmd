5eeff43e109835e673211b3aeadc8598e8279f46
Sergey Savchuk <savchuk.sergey@gmail.com>
2017-11-07 12:39:49 +0200

    build

--- Artifacts/release.cmd
+++ Artifacts/release.cmd
@@ -1,17 +1,11 @@
 @echo off
-set atmasourcepath=../Atmega.Asm/Bin/Debug/netcoreapp2.0
-set atmatargetpath=release/atma
 
 rd release /S /Q
 md release
 
 cd release
 md atma
-cd ..
 
-xcopy /y "%atmasourcepath%\atma.dllexe" "%atmatargetpath%\*"
-xcopy /y "%atmasourcepath%\atma.dll.config" "%atmatargetpath%\*"
-xcopy /y "%atmasourcepath%\atmega.dll" "%atmatargetpath%\*"
-xcopy /y "%atmasourcepath%\inc" "%atmatargetpath%\inc\*"
-
-pause
+cd ../../Atmega.Asm
+dotnet publish -c Release -r win10-x64 -o ../Artifacts/release/atma
+cd ../Artifcats
