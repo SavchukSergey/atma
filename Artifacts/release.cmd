66681a21b559609f3de5434752b98f7ca29263e3
Sergey Savchuk <savchuk.sergey@gmail.com>
2017-11-07 12:10:35 +0200

    new csproj

--- Artifacts/release.cmd
+++ Artifacts/release.cmd
@@ -1,5 +1,5 @@
 @echo off
-set atmasourcepath=../Atmega.Asm/Bin/Debug
+set atmasourcepath=../Atmega.Asm/Bin/Debug/netcoreapp2.0
 set atmatargetpath=release/atma
 
 rd release /S /Q
@@ -9,8 +9,8 @@ cd release
 md atma
 cd ..
 
-xcopy /y "%atmasourcepath%\atma.exe" "%atmatargetpath%\*"
-xcopy /y "%atmasourcepath%\atma.exe.config" "%atmatargetpath%\*"
+xcopy /y "%atmasourcepath%\atma.dllexe" "%atmatargetpath%\*"
+xcopy /y "%atmasourcepath%\atma.dll.config" "%atmatargetpath%\*"
 xcopy /y "%atmasourcepath%\atmega.dll" "%atmatargetpath%\*"
 xcopy /y "%atmasourcepath%\inc" "%atmatargetpath%\inc\*"
 
